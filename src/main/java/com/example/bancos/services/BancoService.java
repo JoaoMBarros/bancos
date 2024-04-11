02593822c10ce4710b5553377f5b45b9dbe2f14b
João Pedro <joao2015.76@gmail.com>
2024-04-11 19:30:54 -0300

    feature: get all bancos and get specific banco

--- src/main/java/com/example/bancos/services/BancoService.java
+++ src/main/java/com/example/bancos/services/BancoService.java
@@ -1,5 +1,25 @@
 package com.example.bancos.services;
 
 
+import com.example.bancos.domains.Banco;
+import com.example.bancos.repositories.BancoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class BancoService {
+
+    @Autowired
+    private BancoRepository repository;
+
+    public Optional<Banco> getSpecificBanco(Integer codigo) throws Exception {
+        return repository.findBancoByCodigo(codigo);
+    }
+
+    public List<Banco> getAllBancos(){
+        return repository.findAll();
+    }
 }
